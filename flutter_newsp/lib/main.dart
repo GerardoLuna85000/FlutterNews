378581c0b35bfc205617a64195dc33f70c7f4130
Gerardo Luna <gdlunaa109@gmail.com>
2021-11-09 15:47:44 -0600

    fisrt commit

---
+++ flutter_newsp/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:news_app_api/pages/home.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en','US'),
+        const Locale('es','ES'),
+      ],
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: HomePage(),
+    );
+  }
+}
